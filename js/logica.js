34663ced674dac1aa107463687a2d5cf2bb6fb63
Fausto Fernandez <fausto44190084@gmail.com>
2024-03-18 16:35:46 -0300

    Corregi la logica

--- js/logica.js
+++ js/logica.js
@@ -24,18 +24,18 @@ function resetearCronometro() {
 }
 
 function actualizarCronometro() {
-    let ahora = new Date();
-    let horas = ahora.getHours();
-    let minutos = ahora.getMinutes();
-    let segundos = ahora.getSeconds();
+    let segundos = tiempo % 60;
+    let minutos = Math.floor(tiempo / 60);
+    let horas = Math.floor(minutos / 60);
+    minutos %= 60;
+    horas %= 60;
 
-    horas = horas < 10 ? "0" + horas : horas;
-    minutos = minutos < 10 ? "0" + minutos : minutos;
     segundos = segundos < 10 ? "0" + segundos : segundos;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    horas = horas < 10 ? "0" + horas : horas;
 
     cronometro.textContent = horas + ":" + minutos + ":" + segundos;
 }
-
 
 document.addEventListener("DOMContentLoaded", function() {
     cronometro = document.getElementById("cronometro");
